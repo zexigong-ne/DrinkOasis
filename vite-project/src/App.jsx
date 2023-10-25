d3ff01320813c7bcf219202fc4966a872e29604b
RebeccaYHZ <zhang.yah@northeastern.edu>
2023-10-24 22:31:46 -0700

    import react router and create some jsx files.

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import 
 import './App.css'
 
 function App() {
