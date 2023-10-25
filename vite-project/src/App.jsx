9055acc7a9e0172f5f1a69a81253831604100c55
RebeccaYHZ <zhang.yah@northeastern.edu>
2023-10-24 23:06:41 -0700

    Update home.jsx

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
       <nav>
         <ul>
           <li>
-            <Link to="/">Home</Link>
+            <Link to="/">HomePage</Link>
           </li>
         </ul>
       </nav>
