7aaf23dd7dfad30664d33bd9e9ba40436570e0c9
RebeccaYHZ <zhang.yah@northeastern.edu>
2023-10-27 16:37:58 -0700

    import Bootstrap and create css file for Register.

--- vite-project/src/components/Register.jsx
+++ vite-project/src/components/Register.jsx
@@ -1,4 +1,7 @@
 import { useState } from 'react';
+import '../assets/css/Register.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function Register() {
   const [formData, setFormData] = useState({
@@ -24,13 +27,14 @@ function Register() {
   };
 
   return (
-    <div>
-      <h2>Register</h2>
-      <form onSubmit={handleSubmit}>
+    <div className="wrapper">
+      <h2 className="form-register-heading">Register</h2>
+      <form className="form-register" onSubmit={handleSubmit}>
         <div>
           <label>Username</label>
           <input
             type="text"
+            className="form-control"
             name="username"
             value={username}
             onChange={handleChange}
@@ -38,12 +42,13 @@ function Register() {
         </div>
         <div>
           <label>Email</label>
-          <input type="email" name="email" value={email} onChange={handleChange} />
+          <input type="email" className="form-control" name="email" value={email} onChange={handleChange} />
         </div>
         <div>
           <label>Password</label>
           <input
             type="password"
+            className="form-control"
             name="password"
             value={password}
             onChange={handleChange}
@@ -53,12 +58,13 @@ function Register() {
           <label>Confirm Password</label>
           <input
             type="password"
+            className="form-control"
             name="confirmPassword"
             value={confirmPassword}
             onChange={handleChange}
           />
         </div>
-        <button type="submit">Register</button>
+        <button className="btn btn-lg btn-primary btn-block btn-submit" type="submit">Register</button>
       </form>
     </div>
   );
