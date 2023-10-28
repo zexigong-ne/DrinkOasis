05fc980b969af5c4773eb21370f37e86e4cb42d8
RebeccaYHZ <zhang.yah@northeastern.edu>
2023-10-27 17:13:45 -0700

    update Login.jsx with style

--- vite-project/src/components/Login.jsx
+++ vite-project/src/components/Login.jsx
@@ -1,6 +1,7 @@
 import { Component } from 'react';
-// import { withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import '../assets/css/Register.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class Login extends Component {
   constructor() {
@@ -31,24 +32,40 @@ class Login extends Component {
 
   render() {
     return (
+      <div className="wrapper">
+      <h2 className="form-register-heading">Login</h2>
+      <form className="form-register">
       <div>
-        <h2>Login</h2>
-        <input
+          <div className="label-container">
+            <label>Username:</label>
+          </div>
+          <input
           type="text"
+          className="form-control"
           name="username"
           placeholder="Username"
           value={this.state.username}
           onChange={this.handleInputChange}
         />
-        <input
+        </div>
+        
+        <div>
+          <div className="label-container">
+            <label>Password:</label>
+          </div>
+          <input
           type="password"
+          className="form-control"
           name="password"
           placeholder="Password"
           value={this.state.password}
           onChange={this.handleInputChange}
         />
-        <button onClick={this.handleLogin}>Login</button>
+        </div>
+        
+        <button className="btn btn-lg btn-primary btn-block btn-submit" onClick={this.handleLogin}>Login</button>
         <p>{this.state.errorMessage}</p>
+        </form>
       </div>
     );
   }
@@ -58,5 +75,4 @@ Login.propTypes = {
     history: PropTypes.object.isRequired,
 }
 
-// export default withRouter(Login);
 export default Login;
