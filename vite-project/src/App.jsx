4cb45ac238b6634e14e1cc9e908dabb5253c094c
rebeccazzha <110057062+rebeccazzha@users.noreply.github.com>
2023-10-30 23:14:37 -0700

    updated Review.css and try with mock data

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -12,7 +12,31 @@ import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import logoImage from './assets/img/logoImage.png';
 
 function App() {
-  const [reviews, setReviews] = useState([]);
+
+  const sampleReviews = [
+    {
+      barName: "The Drunken Ship",
+      location: "Rome, Italy",
+      review: "Great place to hang out with friends and enjoy a pint. Loved the ambiance and the music. A bit crowded, but that's to be expected!"
+    },
+    {
+      barName: "Skyline Bar",
+      location: "Riga, Latvia",
+      review: "The view from this bar is amazing, especially during sunset. Drinks are a bit pricey, but it's worth it for the experience."
+    },
+    {
+      barName: "Hemingway Bar",
+      location: "Prague, Czech Republic",
+      review: "A cozy place with a classic feel. Great selection of cocktails, and the bartenders are very knowledgeable."
+    },
+    {
+      barName: "The Broken Shaker",
+      location: "Miami, USA",
+      review: "Trendy spot with a vibrant crowd. The outdoor setting is perfect for Miami nights. Don't miss their signature cocktails!"
+    }
+  ];
+
+  const [reviews, setReviews] = useState(sampleReviews);
   const [diaries, setDiaries] = useState([]);
 
   return (
--- vite-project/src/components/Reviews.jsx
+++ vite-project/src/components/Reviews.jsx
@@ -5,13 +5,15 @@ function Reviews({ reviews }) {
 const navigate = useNavigate();
 
 return (
-  <div className="reviews-container">
-      <div className="reviews-header">
+  <div className="main">
+      <div className="title">
           <h1>Bar Reviews</h1>
-          <button className="add-review-btn" onClick={() => navigate("/PostReview")}>Add a Review</button>
+          <div className="post-btn">
+                <button className="add-review-btn" onClick={() => navigate("/PostReview")}>Add a Review</button>
+          </div>
       </div>
       {reviews.map((review, index) => (
-          <div key={index}>
+          <div key={index} className="review">
               <h2>{review.barName}</h2>
               <p>Location: {review.location}</p>
               <p>Review: {review.review}</p>
